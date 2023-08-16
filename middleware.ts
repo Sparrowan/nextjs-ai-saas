2851131c52ed5beaf4117a982f0486813ffd9a7b
Alphius <alphiuswambua@gmail.com>
2023-08-16 13:52:00 +0300

    Added video api and UI generation

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/dashboard", "/conversation", "/api/conversation", "/code", "/api/code", "/image", "/api/image", "/music", "/api/music"]
+  publicRoutes: ["/", "/dashboard", "/conversation", "/api/conversation", "/code", "/api/code", "/image", "/api/image", "/music", "/api/music", "/video", "/api/video"]
 });
 
 export const config = {
