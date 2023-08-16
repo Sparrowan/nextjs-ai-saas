0d9d5edc5bc8a2b2d72d0a2c206e4c65e6f6f34f
Alphius <alphiuswambua@gmail.com>
2023-08-16 12:42:41 +0300

    Added music UI and API

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/dashboard", "/conversation", "/api/conversation", "/code", "/api/code", "/image", "/api/image"]
+  publicRoutes: ["/", "/dashboard", "/conversation", "/api/conversation", "/code", "/api/code", "/image", "/api/image", "/music", "/api/music"]
 });
 
 export const config = {
